9ba092a3eea0269386a4f807abb7c73f662b8a55
rigojr <rigojunnior@gmail.com>
2024-01-23 18:11:34 -0500

    fix: clean up folders and useless files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,10 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
+  'state': {
   },
-  getters: {
+  'mutations': {
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  'actions': {
   }
 })
