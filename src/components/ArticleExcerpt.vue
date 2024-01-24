9fa2ef51904e217ed6a281967cb9c3573c0c1d64
rigojr <rigojunnior@gmail.com>
2024-01-23 22:22:12 -0500

    feat: add article view collection still wip

---
+++ src/components/ArticleExcerpt.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ArticleStatus } from '@/entities';
+
+/**
+ * The component public properties.
+ */
+export interface Props {
+  'id': number;
+  'date': string;
+  'status': ArticleStatus;
+  'title': string;
+  'content': string;
+  'externalLink': string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="article-excerpt">
+    <h2 class="article-excerpt__title">{{ props.title }}</h2>
+  </div>
+</template>
--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -1,10 +1,51 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { ComputedRef, computed, onMounted } from 'vue';
 import { useStore } from 'vuex';
 
+import { Article, ArticleStatus } from '@/entities';
 import { key } from '@/store/';
+import ArticleExcerpt, { Props as ArticleExcerptProps } from '@/components/ArticleExcerpt.vue';
 
 const store = useStore(key);
+const isLoading = computed(() => store.state.session.isArticlesLoading); // TODO: Improve these computed declaration.
+const articles: ComputedRef<ArticleExcerptProps[]> = computed(() => getArticles(store.state.session.articles));
+
+/**
+ * Gets a collection of articles excerpt public properties.
+ *
+ * @param articles The article collection.
+ */
+function getArticles(articles: Article[]): ArticleExcerptProps[] {
+  const publishedArticles = getPublishedArticles(articles);
+
+  return publishedArticles.map((article) => buildArticleExcerptProps(article));
+}
+
+/**
+ * Gets the published articles.
+ *
+ * @param articles The article collection.
+ */
+function getPublishedArticles(articles: Article[]): Article[] {
+  return articles.filter((article ) => article.status === ArticleStatus.PUBLISH);
+}
+
+/**
+ * Builds the article excerpt public properties.
+ *
+ * @param article The article entity.
+ */
+function buildArticleExcerptProps(article: Article): ArticleExcerptProps {
+  // TODO: should not be doing here, find other place.
+  return {
+    'id': article.id,
+    'date': article.date,
+    'status': article.status,
+    'title': article.title.rendered,
+    'content': article.content.rendered,
+    'externalLink': article.link
+  }
+}
 
 onMounted(() => {
   if (store.state.session.articles.length > 0) {
@@ -16,5 +57,21 @@ onMounted(() => {
 </script>
 
 <template>
-  <p>Hello there</p>
+  <div class="articles-view">
+    <p
+      v-if="isLoading"
+      class="articles-view__loading"
+    >
+      Please wait...
+    </p>
+    <div
+      v-else
+      class="articles-view__collection"
+    >
+      <ArticleExcerpt
+        v-for="article in articles"
+        v-bind="article"
+      />
+    </div>
+  </div>
 </template>
